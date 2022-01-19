817e7f5a0d00eda1a275a80be72e450db85d2fed
James Rodden <james.rodden@savanta.com>
2022-01-19 14:20:51 +0000

    Alt text for hero and swoop

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,9 +21,9 @@ const Hero = () => {
             /images/hero-img@3x.jpg 3x
           "
         />
-        <HeroImage src="/images/hero-img.jpg" />
+        <HeroImage src="/images/hero-img.jpg" alt="Cute cat staring towards camera"/>
       </picture>
-      <Swoop src="/swoop.svg" />
+      <Swoop src="/swoop.svg" alt="Decorative wavy border" />
     </Wrapper>
   );
 };
